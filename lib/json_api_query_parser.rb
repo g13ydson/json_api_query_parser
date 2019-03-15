204d0919ef5e88c45fae88c16b45588b1eadfc33
Gleydson Tavares <tavares.gleydson@gmail.com>
2019-03-15 09:29:45 -0300

    modifies variables and methods for snake_case

--- lib/json_api_query_parser.rb
+++ lib/json_api_query_parser.rb
@@ -4,94 +4,94 @@ require "json_api_query_parser/version"
 
 module JsonApiQueryParser
   PARSE_PARAM = {
-    parseInclude: /^include\=(.*?)/i,
-    parseFields: /^fields\[(.*?)\]\=.*?$/i,
-    parsePage: /^page\[(.*?)\]\=.*?$/i
+    parse_include: /^include\=(.*?)/i,
+    parse_fields: /^fields\[(.*?)\]\=.*?$/i,
+    parse_page: /^page\[(.*?)\]\=.*?$/i
   }.freeze
 
-  def self.parseRequest(url)
-    requestData = {
-      resourceType: nil,
+  def self.parse_request(url)
+    request_data = {
+      resource_type: nil,
       identifier: nil,
-      queryData: {
+      query_data: {
         include: [],
         fields: {},
         page: {}
       }
     }
 
-    urlSplit = url.split("?")
-    requestData = parseEndpoint(urlSplit[0], requestData) if urlSplit[0]
+    url_split = url.split("?")
+    request_data = parse_endpoint(url_split[0], request_data) if url_split[0]
 
-    if urlSplit[1]
-      requestData[:queryData] = parseQueryParameters(urlSplit[1], requestData[:queryData])
+    if url_split[1]
+      request_data[:query_data] = parse_query_parameters(url_split[1], request_data[:query_data])
     end
 
-    requestData
+    request_data
   end
 
-  def self.parseQueryParameters(queryString, requestData)
-    querySplit = queryString.split("&")
+  def self.parse_query_parameters(query_string, request_data)
+    query_split = query_string.split("&")
 
-    querySplit.each do |query|
-      delegateToParser(query, requestData)
+    query_split.each do |query|
+      delegate_to_parser(query, request_data)
     end
 
-    requestData
+    request_data
   end
 
-  def self.parseEndpoint(endpointString, requestData)
-    requestSplit = endpointString.split("/")
+  def self.parse_endpoint(endpoint_string, request_data)
+    request_split = endpoint_string.split("/")
 
-    requestData[:resourceType] = requestSplit[0]
-    requestData[:identifier] = requestSplit.length >= 2 ? requestSplit[1] : nil
+    request_data[:resource_type] = request_split[0]
+    request_data[:identifier] = request_split.length >= 2 ? request_split[1] : nil
 
-    requestData
+    request_data
   end
 
-  def self.delegateToParser(query, requestData)
-    PARSE_PARAM.each do |functionName, _value|
-      if query =~ PARSE_PARAM[functionName.to_sym]
-        requestData = send(functionName, query, requestData)
+  def self.delegate_to_parser(query, request_data)
+    PARSE_PARAM.each do |function_name, _value|
+      if query =~ PARSE_PARAM[function_name.to_sym]
+        request_data = send(function_name, query, request_data)
       end
     end
   end
 
-  def self.parseInclude(includeString, requestData)
-    targetString = includeString.split("=")[1]
-    requestData[:include] = targetString.split(",")
+  def self.parse_include(include_string, request_data)
+    target_string = include_string.split("=")[1]
+    request_data[:include] = target_string.split(",")
 
-    requestData
+    request_data
   end
 
-  def self.parseFields(fieldsString, requestData)
-    targetResource, targetFields, targetFieldsString = ""
-    fieldNameRegex = /^fields.*?\=(.*?)$/i
+  def self.parse_fields(fields_string, request_data)
+    target_resource, target_fields, target_fields_string = ""
+    field_name_regex = /^fields.*?\=(.*?)$/i
 
-    targetResource = fieldsString.scan(PARSE_PARAM[:parseFields])
+    target_resource = fields_string.scan(PARSE_PARAM[:parse_fields])
 
-    targetFieldsString = fieldsString.scan(fieldNameRegex)
+    target_fields_string = fields_string.scan(field_name_regex)
 
-    requestData[:fields][targetResource[0][0]] = !requestData[:fields][targetResource[0][0]] ? [] : targetResource[0][0]
-    targetFields = targetFieldsString[0][0].split(",")
+    request_data[:fields][target_resource[0][0]] = !request_data[:fields][target_resource[0][0]] ? [] : target_resource[0][0]
+    target_fields = target_fields_string[0][0].split(",")
 
-    targetFields.each do |targetField|
-      requestData[:fields][targetResource[0][0]] << targetField
+    target_fields.each do |targetField|
+      request_data[:fields][target_resource[0][0]] << targetField
     end
 
-    requestData
+    request_data
   end
 
-  def self.parsePage (pageString, requestData)
-    pageSettingKey, pageSettingValue = ""
-    pageValueRegex = /^page.*?\=(.*?)$/i
+  def self.parse_page (page_string, request_data)
+    page_setting_key, page_setting_value = ""
+    page_value_regex = /^page.*?\=(.*?)$/i
 
-    pageSettingKey = pageString.scan(PARSE_PARAM[:parsePage])
+    page_setting_key = page_string.scan(PARSE_PARAM[:parse_page])
 
-    pageSettingValue = pageString.scan(pageValueRegex)
+    page_setting_value = page_string.scan(page_value_regex)
 
-    requestData[:page][pageSettingKey[0][0]] = pageSettingValue[0][0]
+    request_data[:page][page_setting_key[0][0]] = page_setting_value[0][0]
 
-    requestData
+    request_data
   end
 end
